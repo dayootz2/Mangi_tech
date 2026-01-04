ff0004aa34d51c410c40f0ef184b88936a03aea0
MANGITECH <dayoomasami@gmail.com>
2026-01-04 21:53:42 +0300

    Change caption to 'MANGI VIRUS™' in Instagram messages

--- loftcore/instagram.js
+++ loftcore/instagram.js
@@ -113,12 +113,12 @@ async function instagramCommand(sock, chatId, message) {
                     await sock.sendMessage(chatId, {
                         video: { url: mediaUrl },
                         mimetype: "video/mp4",
-                        caption: "ʟᴏꜰᴛ Qᴜᴀɴᴛᴜᴍ™"
+                        caption: "MANGI VIRUS™"
                     }, { quoted: message });
                 } else {
                     await sock.sendMessage(chatId, {
                         image: { url: mediaUrl },
-                        caption: "ʟᴏꜰᴛ Qᴜᴀɴᴛᴜᴍ™"
+                        caption: "MANGI VIRUS™"
                     }, { quoted: message });
                 }
                 
