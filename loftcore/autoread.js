fa00b36cd007950e5dc2a868b532ff81ced29c96
MANGITECH <dayoomasami@gmail.com>
2026-01-06 23:57:24 +0300

    Update autoread.js

--- loftcore/autoread.js
+++ loftcore/autoread.js
@@ -32,7 +32,7 @@ async function autoreadCommand(sock, chatId, message) {
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '120363398106360290@newsletter',
-                        newsletterName: 'mangi'                        serverMessageId: -1
+                        newsletterName: ''                        serverMessageId: -1
                     }
                 }
             });
