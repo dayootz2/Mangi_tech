e7b30963277fd8e5dcdd3be70cb80356a3d2c10d
MANGITECH <dayoomasami@gmail.com>
2026-01-07 11:28:25 +0300

    Update newsletter JID in index.js

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ async function startLoftBase() {
                             forwardingScore: 1,
                             isForwarded: true,
                             forwardedNewsletterMessageInfo: {
-                                newsletterJid: '120363398106360290@newsletter',
+                                newsletterJid: '120363404000266260@newsletter',
                                 newsletterName: 'MANGI VIRUS™',
                                 serverMessageId: -1
                             }
@@ -258,7 +258,7 @@ async function startLoftBase() {
                         forwardingScore: 1,
                         isForwarded: true,
                         forwardedNewsletterMessageInfo: {
-                            newsletterJid: '120363398106360290@newsletter',
+                            newsletterJid: '120363404000266260@newsletter',
                             newsletterName: 'MANGI VIRUS™',
                             serverMessageId: -1
                         }
