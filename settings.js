da2b5a0723d09c155b86945769d1578b7dd17cf3
MANGITECH <dayoomasami@gmail.com>
2026-01-08 12:04:57 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ const settings = {
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.5",
-  updateZipUrl: "https://github.com/xmdloft23/loft-quantum/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/dayootz2/Mangi_tech/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
